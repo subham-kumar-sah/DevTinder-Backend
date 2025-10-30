7e9966b7cac8510eff4b912f3a4b391226a28a9e
Subham Kumar Sah <sahsubham31@gmail.com>
2025-10-30 19:22:36 +0530

    xploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -5,16 +5,32 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
+// app.use("/user", (req, res) => {
+//   res.send("This is User route middleware");
+// });
+
+app.get("/user", (req, res) => {
+  res.send("This is GET User route");
+});
+
+app.post("/user", (req, res) => {
+  res.send("This is POST User route");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("This is DELETE User route");
+});
+
 // Middleware to handle all incoming requests for path "/"
 
-app.use("/hello", (req, res) => {
-  res.send("This is the hello page");
-});
+// app.use("/hello", (req, res) => {
+//   res.send("This is the hello page");
+// });
 
-app.use("/test", (req, res) => {
-  res.send("This is a test");
-});
+// app.use("/test", (req, res) => {
+//   res.send("This is a test");
+// });
 
-app.use("/", (req, res) => {
-  res.send("Welcome to the homepage");
-});
+// app.use("/", (req, res) => {
+//   res.send("Welcome to the homepage");
+// });
