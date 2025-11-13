65479545d93d182b7590b3a169dacbdbcffe07f4
Subham Kumar Sah <sahsubham31@gmail.com>
2025-11-13 20:17:47 +0530

    GET APIs

--- src/app.js
+++ src/app.js
@@ -3,20 +3,15 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+//Middleware for coverting data into json
+app.use(express.json());
+
 app.post("/signUp", async (req, res) => {
   // Sign up logic here
-  const userData = {
-    firstName: "Subham",
-    lastName: "Sah",
-    email: "subham.sah@gmail.com",
-    password: "securepassword",
-    age: 29,
-  };
-  //Creating instance of User model
   try {
     //Creating Instance of User model
-    const user = new User(userData);
-    //Saving user to database as user.save() returns a promise we use await
+    const user = new User(req.body);
+    //Saving user to database as user.save() returns a promise so we use await
     await user.save();
     res.send("User signed up");
   } catch (err) {
@@ -24,8 +19,27 @@ app.post("/signUp", async (req, res) => {
   }
 });
 
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user.length !== 0) res.send(user);
+    else res.status(400).send("User not found");
+  } catch (err) {
+    res.status(400).send("User Not Found");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Users not found");
+  }
+});
+
 connectDB()
-  .then(() => {
+  .then(async () => {
     console.log("Database connected successfully");
     app.listen(3000, () => {
       console.log("Server is running on port 3000");
--- src/models/user.js
+++ src/models/user.js
@@ -7,7 +7,7 @@ const { Schema, model } = mongoose;
 const userSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true },
   password: { type: String, required: true },
   age: { type: Number, required: true },
   gender: { type: String, required: false },
